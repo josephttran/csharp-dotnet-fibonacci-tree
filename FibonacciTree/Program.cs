15e65cf07f2fa568a5e8769112a76c79ca5f980b
josephttran <42635012+josephttran@users.noreply.github.com>
2019-05-14 22:48:54 -0700

    Clean up Main function

--- FibonacciTree/Program.cs
+++ FibonacciTree/Program.cs
@@ -14,24 +14,15 @@ namespace FibonacciTree
             Console.Write(", ");
         }
 
-        static void Main(string[] args)
+        static void PrintFibonacciDetails(FibTree fibTree)
         {
-            int number = 6;
-
-            List<int> fibonacciList = Fibonacci.FibSequence(number);
-            string s = String.Join(", ", fibonacciList.ToArray());
-            Console.WriteLine($"Fibonacci Sequence of {number} is: ");
-            Console.WriteLine(s);
-
-            FibTree fibTree = new FibTree(number);
-
             fibTree.PrintTreeLeftToRight();
 
             Console.WriteLine($"\nFibonacci tree height is {fibTree.GetHeight()}");
 
             Node r = fibTree.GetRoot();
             Console.WriteLine($"Root contain the number {r.GetItem()}");
-            
+
             if (r.GetLeftChild() != null)
             {
                 Console.WriteLine($"Root left child contain the number {r.GetLeftChild().GetItem()}");
@@ -44,6 +35,23 @@ namespace FibonacciTree
             fibTree.LevelOrderTraverse();
 
             fibTree.InOrderTraverse(VisitItem);
+        }
+
+        static void PrintFibonacciList(List<int> fibonacciList, int number)
+        {
+            string s = String.Join(", ", fibonacciList.ToArray());
+            Console.WriteLine($"Fibonacci Sequence of {number} is: ");
+            Console.WriteLine(s);
+        }
+        static void Main(string[] args)
+        {
+            int number = 6;
+
+            List<int> fibonacciList = Fibonacci.FibSequence(number);
+            FibTree fibTree = new FibTree(number);
+
+            PrintFibonacciList(fibonacciList, number);
+            PrintFibonacciDetails(fibTree);
 
             Console.WriteLine("\n\nHello World!");
         }
